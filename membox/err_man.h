48951c4408ebabc028369294990508a4965141f7
eleonora di gregorio <eleonora.dgr8@gmail.com>
2016-05-11 15:53:13 +0200

    error

---
+++ membox/err_man.h
@@ -0,0 +1,32 @@
+#if !defined(MEMBOX_ERRMAN_)
+#define MEMBOX_ERRMAN_
+
+#define ec_meno1(s,m) \
+	if((s)==-1) { perror (m); exit(EXIT_FAILURE);} //exit failure ha senso?
+#define ec_null(s,m) \
+	if((s)==NULL){ perror(m); exit(EXIT_FAILURE);}
+
+#define ec_meno1_c(s,m,c)\
+	if((s)==-1){ perror(m); c;}
+#define ec_null_c(s,m,c)\
+	if((s)==NULL){ perror(m); c;}
+
+
+#define Pthread_create(pthread_t* thread_id, const pthread_attr_t *attr, void* (*start_fnc) (void*), void* arg) \
+	if((int cod=pthread_create (thread_id, attr,start_fnc, arg))!=0){ printf("error in pthread create: %d\n",cod);exit(EXIT_FAILURE);}
+
+#define Pthread_mutex_lock( pthread_mutex_t* mutex) \
+	if((int cod=pthread_mutex_lock(mutex))!=0){ printf("error in mutex lock:%d\n",cod); exit(EXIT_FAILURE);}
+
+#define Pthread_mutex_unlock( pthread_mutex_t* mutex)\
+	if((int cod=pthread_mutex_unlock(mutex))!=0){ printf("error in mutex unlock:%d\n",cod); exit(EXIT_FAILURE);}
+
+#define Pthread_cond_wait( pthread_cond_t* cond, pthread_mutex_t* mtx) \
+	if((int cod=pthread_cond_wait(cond,mtx))!=0){ printf("error in cond wait:%d\n",cod); exit(EXIT_FAILURE);}
+
+#define Pthread_cond_signal( pthread_cond_t* cond)\
+	if((int cod=pthread_cond_signal(cond))!=0){ printf("error in cond signal:%d\n",cod); exit(EXIT_FAILURE);}
+
+
+//atexit da registrare
+#endif /* MEMBOX_ERRMAN_ */
