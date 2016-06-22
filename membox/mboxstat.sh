f86d206fdf45995a856f26b4f8743acee47a9716
burlamix <burlamix@gmail.com>
2016-06-22 17:05:46 +0200

    script completato

--- membox/mboxstat.sh
+++ membox/mboxstat.sh
@@ -21,96 +21,135 @@ for n in $s ; do
 	if [ $i -eq 8 ] ; then  printf "n. di REMOVE_OP fallite = %s\n" "$n" ;fi
 	if [ $i -eq 9 ] ; then  printf "n. di LOCK_OP ricevute = %s\n" "$n" ;fi
 	if [ $i -eq 10 ] ; then  printf "n. di LOCK_OP fallite = %s\n" "$n" ;fi
-	if [ $i -eq 11 ] ; then  printf "n. massimo di connessioni concorrenti raggiunte = %s\n" "$n" ;fi
-	if [ $i -eq 12 ] ; then  printf "n. massimo di oggetti memorizzati raggiunto = %s\n" "$n" ;fi
-	if [ $i -eq 13 ] ; then  printf "n. corrente di oggetti memorizzati = %s\n" "$n" ;fi
-	if [ $i -eq 14 ] ; then  printf "size massima in bytes raggiunta = %s\n" "$n" ;fi
-	if [ $i -eq 15 ] ; then  printf "size in bytes corrente = %s\n" "$n" ;fi
+	if [ $i -eq 11 ] ; then  printf "n. massimo di connessioni concorrenti raggiunte = %s\n" "$n" ;fi   #  il numero massimo o il numero attuale?
+	if [ $i -eq 12 ] ; then  printf "size in bytes corrente = %s\n" "$n" ;fi
+	if [ $i -eq 13 ] ; then  printf "size massima in bytes raggiunta = %s\n" "$n" ;fi
+	if [ $i -eq 14 ] ; then  printf "n. corrente di oggetti memorizzati = %s\n" "$n" ;fi
+	if [ $i -eq 15 ] ; then  printf "n. massimo di oggetti memorizzati raggiunto = %s\n" "$n" ;fi
+
 	let i=i+1
 done
 
 echo 
+if [ $2 ] ; then
 
-l=${@%$1}" "$1
+	PARSED_OPTIONS=$(getopt -n "$0"  -o psugrcsom --long "help"  -- "$@")
 
-pp=0;uu=0;gg=0;rr=0;cc=0;ss=0;oo=0;mm=0;hh=0
+	eval set -- "$PARSED_OPTIONS"
 
-while getopts ":p:s" ops $l; do
-  case $ops in
-        p)
-            # echo  opzione p
-			pp=1
-            ;;
-        u)
-            # echo  opzione u
-            uu=1
-            ;;
-        g)
-            # echo  opzione g
-            gg=1
-            ;;
-        r)
-            # echo  opzione r
-            rr=1
-            ;;
-        c)
-            # echo  opzione c
-            cc=1
-            ;;
-        s)
-            # echo  opzione s
-            ss=1
-            ;;
-        o)
-            # echo  opzione o
-            oo=1
-            ;;
-        m)
-            # echo  opzione m
-            mm=1
-            ;;  
-        help)
-            # echo  opzione help
-            hh=1
-            ;;  
-  esac
-done
+	while true;
+	do
+	  case $1 in
+	        -p)
+	            # echo  opzione p
+				pp=1
+	                  shift;;
+	        -u)
+	            # echo  opzione u
+	            uu=1
+	                  shift;;
+	        -g)
+	            # echo  opzione g
+	            gg=1
+	                  shift;;
+	        -r)
+	            # echo  opzione r
+	            rr=1
+	                  shift;;
+	        -c)
+	            # echo  opzione c
+	            ccc=1
+	                  shift;;
+	        -s)
+	            # echo  opzione s
+	            ss=1
+	                  shift;;
+	        -o)
+	            # echo  opzione o
+	            oo=1
+	                  shift;;
+	        -m)
+	            # echo  opzione m
+	            mm=1
+	                  shift;;
+	        --help)
+	             #echo  opzione aiuto
+	            hh=1
+	                  shift;;
+	        \?)
+     			 echo opzione non conosciuta
+      			      shift;;
+   			 --)
+ 			     shift
+  			    break;;
+	  esac
+	done
 
-while read data trat a b c d e f g h i l m n o p q ; do
+	max_a_nc=0
+	max_a_s=0
+	max_a_ng=0
 
-	printf "\nle statistice del timestamp all'orario %s erano\n" "$data" 
+	while read data trat put_o put_f up_o up_f get_o get_f rem_o rem_f lk_o lk_f nc siz max_s n_ogg max_ng ; do
 
-	if [ $pp -eq 1 ]; then  
-		printf "n. di PUT_OP ricevute = %s\n" "$a" 
-		printf "n. di PUT_OP fallite = %s\n" "$b" 
-	fi
+		printf "\nle statistice del timestamp all'orario %s erano\n" "$data" 
 
-	if [ $uu -eq 1 ]; then  
-		printf "n. di UPDATE_OP ricevute = %s\n" "$c" 
-		printf "n. di UPDATE_OP fallite = %s\n" "$d" 
-	fi
+		if [ $pp -eq 1 ]; then  
+			printf "n. di PUT_OP ricevute = %s\n" "$put_o" 
+			printf "n. di PUT_OP fallite = %s\n" "$put_f" 
+		fi
 
-	if [ $gg -eq  1 ]; then  
-		printf "n. di GET_OP ricevute = %s\n" "$e" 
-		printf "n. di GET_OP fallite = %s\n" "$f" 
-	fi
+		if [ $uu -eq 1 ]; then  
+			printf "n. di UPDATE_OP ricevute = %s\n" "$up_o" 
+			printf "n. di UPDATE_OP fallite = %s\n" "$up_f" 
+		fi
 
-	if [ $rr -eq  1 ]; then  
-		printf "n. di REMOVE_OP ricevute = %s\n" "$n" 
-		printf "n. di REMOVE_OP fallite = %s\n" "$n" 
-	fi
+		if [ $gg -eq  1 ]; then  
+			printf "n. di GET_OP ricevute = %s\n" "$get_o" 
+			printf "n. di GET_OP fallite = %s\n" "$get_f" 
+		fi
 
-	if [ $oo -eq  1 ]; then  
-		printf "n. corrente di oggetti memorizzati = %s\n" "$n" 
-	fi
+		if [ $rr -eq  1 ]; then  
+			printf "n. di REMOVE_OP ricevute = %s\n" "$rem_o" 
+			printf "n. di REMOVE_OP fallite = %s\n" "$rem_f" 
+		fi
 
-	if [ $mm -eq  1 ]; then  
-		printf "n. massimo di connessioni concorrenti raggiunte = %s\n" "$n"
-		printf "n. massimo di oggetti memorizzati raggiunto = %s\n" "$n"
-		printf "size massima in bytes raggiunta = %s\n" "$n" 
-	fi
+		if [ $ccc -eq  1 ]; then  
+			printf "n. massimo di connessioni concorrenti raggiunte = %s\n" "$nc" 
+		fi
 
+		siz_KB=$((siz * 1000))
+		if [ $ss -eq  1 ]; then  
+			printf "size attuale della repository in KB = %s\n" "$siz_KB"
+		fi
 
-done < $1
+		if [ $oo -eq  1 ]; then  
+			printf "n. corrente di oggetti memorizzati = %s\n" "$n_ogg" 
+		fi
 
-echo
+		if [ "$max_a_nc" -lt "$nc" ] ; then
+			max_a_nc=$nc
+		fi
+
+		if [ "$max_a_s" -lt "$max_s" ] ; then
+			max_a_s=$max_s
+		fi
+
+		if [ "$max_a_ng" -lt "$max_ng" ] ; then
+			max_a_ng=$max_ng
+		fi
+
+	done < $1
+
+		if [ $mm -eq  1 ]; then  
+			printf "n. massimo di connessioni concorrenti raggiunte = %s\n" "$max_a_nc"
+
+			printf "n. massimo di oggetti memorizzati raggiunto = %s\n" "$max_a_ng"
+
+			siz_KB=$((max_a_s * 1000))
+			printf "size massima in KB raggiunta = %s\n" "$siz_KB" 
+		fi
+
+		if [ $hh -eq  1 ]; then  
+			echo questo è il messaggio di help che ti aiuterà a capire come usare questo script
+		fi
+fi
