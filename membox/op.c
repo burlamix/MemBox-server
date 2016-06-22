edf8101152026e626b0dd61ed09f1964425905e8
burlamix <burlamix@gmail.com>
2016-06-22 21:32:10 +0200

    allocazione statica hdr

--- membox/op.c
+++ membox/op.c
@@ -15,7 +15,6 @@ int MaxObjSize= 100;
 
 int sendReply(long fd, message_hdr_t *hdr){
   write(fd, hdr ,sizeof(message_hdr_t));
-  free(hdr);
   return 0;
 }
 
@@ -39,7 +38,7 @@ int SizeOfRep( icl_hash_t* repo){
 
 int put_op(message_t* new_data,  icl_hash_t* repository,  int fd, struct statistics *mboxStats, pthread_mutex_t lk_stat ){
   
-  message_hdr_t *risp=calloc(1,sizeof(message_hdr_t));
+  message_hdr_t risp;
   message_data_t *obj=calloc(1,sizeof(message_data_t));
   Pthread_mutex_lock(&lk_stat);
   mboxStats->nput++;
@@ -49,8 +48,8 @@ int put_op(message_t* new_data,  icl_hash_t* repository,  int fd, struct statist
 
   //si verifica che la repository non abbia raggiunto il massimo numero di elementi
   if ( repository->StorageSize!=0 && curr_obj >= repository->StorageSize){
-    risp->op= OP_PUT_TOOMANY;
-    sendReply( fd, risp);
+    risp.op= OP_PUT_TOOMANY;
+    sendReply( fd, &risp);
     Pthread_mutex_lock(&lk_stat);
     mboxStats->nput_failed++;
     Pthread_mutex_unlock(&lk_stat);
@@ -58,8 +57,8 @@ int put_op(message_t* new_data,  icl_hash_t* repository,  int fd, struct statist
   }
   //la condizione sulla dimensione della repository viene controllata prima di fare l'inserimento
   if(repository->StorageByteSize!=0 && newdim> repository->StorageByteSize){
-    risp->op= OP_PUT_REPOSIZE;
-    sendReply( fd, risp);
+    risp.op= OP_PUT_REPOSIZE;
+    sendReply( fd, &risp);
     Pthread_mutex_lock(&lk_stat);
     mboxStats->nput_failed++;
     Pthread_mutex_unlock(&lk_stat);
@@ -69,8 +68,8 @@ int put_op(message_t* new_data,  icl_hash_t* repository,  int fd, struct statist
   int op;
 
   if (repository->MaxObjSize!=0 && new_data->data.len > repository->MaxObjSize){
-    risp->op= OP_PUT_SIZE;
-    sendReply( fd, risp);
+    risp.op= OP_PUT_SIZE;
+    sendReply( fd, &risp);
     Pthread_mutex_lock(&lk_stat);
     mboxStats->nput_failed++;
     Pthread_mutex_unlock(&lk_stat);
@@ -82,7 +81,7 @@ int put_op(message_t* new_data,  icl_hash_t* repository,  int fd, struct statist
   op=icl_hash_insert( repository, new_data->hdr.key, obj);
   switch (op){
     case 0 :
-      risp->op= OP_OK;
+      risp.op= OP_OK;
       Pthread_mutex_lock(&lk_stat);
       mboxStats->current_size=newdim;
       if(newdim>mboxStats->max_size)
@@ -94,30 +93,30 @@ int put_op(message_t* new_data,  icl_hash_t* repository,  int fd, struct statist
       break;
     case -1 :
     case -3 :
-    	risp->op= OP_FAIL;
+    	risp.op= OP_FAIL;
       	Pthread_mutex_lock(&lk_stat);
     	mboxStats->nput_failed++;
     	Pthread_mutex_unlock(&lk_stat);
       	break;
     case -2 :
-    	risp->op= OP_PUT_ALREADY;
+    	risp.op= OP_PUT_ALREADY;
     	Pthread_mutex_lock(&lk_stat);
     	mboxStats->nput_failed++;
     	Pthread_mutex_unlock(&lk_stat);
     	break;
   }
-  sendReply( fd, risp);
+  sendReply( fd, &risp);
   return 0;
 }
 
 int update_op(message_t* new_mex, icl_hash_t* repository, int fd, struct statistics  *mboxStats, pthread_mutex_t lk_stat){
 
   message_data_t* dato = (message_data_t*) icl_hash_find( repository, new_mex->hdr.key);
-  message_hdr_t *risp=calloc(1,sizeof(message_hdr_t));
+  message_hdr_t risp;
   
   if(dato==NULL){
-    risp->op= OP_UPDATE_NONE;
-    sendReply( fd, risp);
+    risp.op= OP_UPDATE_NONE;
+    sendReply( fd, &risp);
     Pthread_mutex_lock(&lk_stat);
     mboxStats->nupdate++;
     mboxStats->nupdate_failed++;
@@ -125,7 +124,7 @@ int update_op(message_t* new_mex, icl_hash_t* repository, int fd, struct statist
     return 0;
   }
   if (dato->len != new_mex->data.len){
-    risp->op= OP_UPDATE_SIZE; 
+    risp.op= OP_UPDATE_SIZE; 
     Pthread_mutex_lock(&lk_stat);
     mboxStats->nupdate++;
     mboxStats->nupdate_failed++;
@@ -136,35 +135,35 @@ int update_op(message_t* new_mex, icl_hash_t* repository, int fd, struct statist
     memcpy((void*)dato->buf,(void*)new_mex->data.buf,dato->len);  // qui non va fatto così ma senno
     // free(new_mex);
 
-    risp->op= OP_OK;
+    risp.op= OP_OK;
     Pthread_mutex_lock(&lk_stat);
     mboxStats->nupdate++;
 	Pthread_mutex_unlock(&lk_stat);
 
   }
-  sendReply( fd, risp);
+  sendReply( fd, &risp);
   return 0;
 }
 
 int remove_op(icl_hash_t* repository, membox_key_t key,int fd, struct statistics  *mboxStats, pthread_mutex_t lk_stat){
 
   int op= icl_hash_delete( repository, key);
-   message_hdr_t *risp=calloc(1,sizeof(message_hdr_t));
+   message_hdr_t risp;
   if (op>=0){
-  	risp->op= OP_OK;
+  	risp.op= OP_OK;
     Pthread_mutex_lock(&lk_stat);
     mboxStats->nremove++;
     mboxStats->current_size-=op;
     mboxStats->current_objects--;
 	Pthread_mutex_unlock(&lk_stat);
 	}else{
-    	risp->op=OP_REMOVE_NONE;
+    	risp.op=OP_REMOVE_NONE;
     	Pthread_mutex_lock(&lk_stat);
     	mboxStats->nremove++;
     	mboxStats->nremove_failed++;
 		Pthread_mutex_unlock(&lk_stat);
   }
-    sendReply( fd, risp);
+    sendReply( fd, &risp);
   return 0;
 }
 
@@ -174,24 +173,24 @@ int get_op(icl_hash_t* repository, membox_key_t key,int fd, struct statistics  *
 
     dato= (message_data_t*) icl_hash_find( repository, key);
 
-    message_hdr_t *risp=calloc(1,sizeof(message_hdr_t));
-    risp->key=key;
+    message_hdr_t risp;
+    risp.key=key;
     if (dato==NULL){
-     	risp->op=OP_GET_NONE;
+     	risp.op=OP_GET_NONE;
       	Pthread_mutex_lock(&lk_stat);
     	mboxStats->nget++;
     	mboxStats->nget_failed++;
 		  Pthread_mutex_unlock(&lk_stat);
     }else{
-    	risp->op=OP_OK;
+    	risp.op=OP_OK;
     	Pthread_mutex_lock(&lk_stat);
     	mboxStats->nget++;
 		  Pthread_mutex_unlock(&lk_stat);
 
     }
-    sendReply(fd, risp);
+    sendReply(fd, &risp);
 
-    if(risp->op==OP_OK){
+    if(risp.op==OP_OK){
       write(fd, &(dato->len ),sizeof(int));
       char * aus;
       aus= dato->buf ;
@@ -212,14 +211,14 @@ int get_op(icl_hash_t* repository, membox_key_t key,int fd, struct statistics  *
 }
 
 int lock_op(int fd,icl_hash_t* repository, struct statistics  *mboxStats, pthread_mutex_t lk_stat ){
-  message_hdr_t *risp=calloc(1,sizeof(message_hdr_t));
+  message_hdr_t risp;
 
   pthread_mutex_lock(& repository->lk_repo);
   if(repository->repo_l){//caso in cui la lock è già presa
       pthread_mutex_unlock(& repository->lk_repo);
 
-      risp->op = OP_LOCKED;
-      sendReply( fd, risp);
+      risp.op = OP_LOCKED;
+      sendReply( fd, &risp);
       Pthread_mutex_lock(&lk_stat);
       mboxStats->nlock++;
       mboxStats->nlock_failed++;
@@ -240,8 +239,8 @@ int lock_op(int fd,icl_hash_t* repository, struct statistics  *mboxStats, pthrea
 
     pthread_mutex_unlock(& repository->lk_job_c);
 
-    risp->op = OP_OK;
-    sendReply( fd , risp);
+    risp.op = OP_OK;
+    sendReply( fd, &risp);
     Pthread_mutex_lock(&lk_stat);
     mboxStats->nlock++;
     Pthread_mutex_unlock(&lk_stat);
@@ -251,7 +250,7 @@ int lock_op(int fd,icl_hash_t* repository, struct statistics  *mboxStats, pthrea
 
 int unlock_op(int fd, icl_hash_t* repository, struct statistics  *mboxStats, pthread_mutex_t lk_stat ){
 
-  message_hdr_t *risp=calloc(1,sizeof(message_hdr_t));
+  message_hdr_t risp;
 
 
     pthread_mutex_lock(& repository->lk_repo);
@@ -259,13 +258,13 @@ int unlock_op(int fd, icl_hash_t* repository, struct statistics  *mboxStats, pth
         repository->repo_l=0;
         pthread_mutex_unlock(&(repository->lk_repo));
 
-        risp->op = OP_OK;
-        sendReply(fd,risp);
+        risp.op = OP_OK;
+        sendReply(fd, &risp);
     }else{//caso in cui la lock non è già stata presa o è stata presa da qualcunaltro
         pthread_mutex_unlock(& repository->lk_repo);
 
-        risp->op = OP_LOCK_NONE;
-        sendReply(fd,risp);
+        risp.op = OP_LOCK_NONE;
+        sendReply(fd, &risp);
     }
   return 0;
 }
